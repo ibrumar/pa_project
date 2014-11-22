98dd1adac9f79d110f2c6e3592e71a50e5c70924
Iuli Brumi <iulianbussiness@gmail.com>
2014-11-22 13:52:29 +0100

    Register file working.
    Problem before write enable goes down. Corrupt write is done.

--- DECODE/register_file.v
+++ DECODE/register_file.v
@@ -1,39 +1,70 @@
-module registerFile#(parameter NUM_REGISTERS = 8, parameter LOG_NUM_REGISTERS=3, parameter WIDTH = 16)
+module register_file#(parameter NUM_REGISTERS = 8, parameter LOG_NUM_REGISTERS=3, parameter WIDTH = 16)
 ( 
   input clk,
   input [LOG_NUM_REGISTERS-1:0]rb, //CAMBIAR LOS CLAUDATORS
-  input ra[LOG_NUM_REGISTERS-1:0],
-  output [WIDTH-1:0] a,
-  output [WIDTH-1:0] b,
+  input [LOG_NUM_REGISTERS-1:0]ra,
+  output reg [WIDTH-1:0] a,
+  output reg [WIDTH-1:0] b,
   input [WIDTH-1:0] d,
   input [LOG_NUM_REGISTERS-1:0] writeAddr,
   input writeEnable, //when write enable, write d into writeAddr
   input reset
   );
 
-  wire writeEnableInternal[NUM_REGISTERS-1:0];
-  
-  wire writenContent = mux
-  
-  //an always block must be created to set to 0 all registers if reset == 3b0
-  //otherwise writeEnableInternal[writeAddr] = d
-  
-  //we should use '=' inside always* blocks
-  
+  //wire signed [NUM_REGISTERS-1:0] extendedReset = reset;
+  wire [NUM_REGISTERS-1:0] writeEnableInternal1;
+  //wire writeEnableInternal2[NUM_REGISTERS-1:0];
+  wire [WIDTH*NUM_REGISTERS - 1:0]concatenated_outputs; //reading
 
+//we don't do this with multiplicative indexing because we 
+//don't want to generate a multiplier outside the ALU  
+  always @(*)
+    begin
+      case(ra)
+        3'b000: a <= concatenated_outputs[15:0];
+        3'b001: a <= concatenated_outputs[31:16];
+        3'b010: a <= concatenated_outputs[47:32];
+        3'b011: a <= concatenated_outputs[63:48];
+        3'b100: a <= concatenated_outputs[79:64];
+        3'b101: a <= concatenated_outputs[95:80];
+        3'b110: a <= concatenated_outputs[111:96];
+        default: a <= concatenated_outputs[127:112];
+      endcase
+    end
+
+
+  always @(*)
+    begin
+      case(rb)
+        3'b000: b <= concatenated_outputs[15:0];
+        3'b001: b <= concatenated_outputs[31:16];
+        3'b010: b <= concatenated_outputs[47:32];
+        3'b011: b <= concatenated_outputs[63:48];
+        3'b100: b <= concatenated_outputs[79:64];
+        3'b101: b <= concatenated_outputs[95:80];
+        3'b110: b <= concatenated_outputs[111:96];
+        default: b <= concatenated_outputs[127:112];
+      endcase
+    end
+  
+  decode3 we_dec
+  ( 
+    .writeEnable(writeEnable),
+    .d(writeAddr),
+    .q(writeEnableInternal1)
+  );
+  
   genvar i;
   generate
     for (i=0; i<NUM_REGISTERS; i=i+1)
       begin: gen_regfile
-        register my_register(.clk(clk), .d(d[i]), .enable(enable), 
-          .reset(reset), .q(q[i]));
-      end
+        register my_register(.clk(clk), .d(d), .enable(writeEnableInternal1[i]), 
+          .reset(reset), 
+          .q(concatenated_outputs[(i + 1)*WIDTH - 1:i*WIDTH]));
+      end //we hope that the indexing doesn't imply multiplying
+          //in hardware
   endgenerate
-  
-   input clk,
-  input [WIDTH-1:0] d,
-  input enable,
-  input reset,
-  output [WIDTH-1:0] q
+
+
 
 endmodule
---
+++ DECODE/register_file_tb.v
@@ -0,0 +1,55 @@
+module register_file_tb();
+wire [15:0]a;
+wire [15:0]b;
+
+
+reg [15:0]d;
+reg clock, reset, writeEnable;
+reg [2:0] writeAddr;
+reg [2:0] ra;
+reg [2:0] rb;
+
+
+initial begin
+  clock=1;
+  d=4'h5;
+  reset=0;
+  writeEnable=1;  
+  writeAddr=1;
+  ra = 1;
+  rb = 8;
+  
+  
+  #20 reset=1;
+  //we should read ra=1 and rb=8
+  
+  #20 d=4'h1;
+  writeEnable=0;  
+  writeAddr=8;
+  ra = 1;
+  rb = 8;
+  //writing should't be allowed and in rb we should read 0
+  
+  #20 writeEnable=1;  
+
+end
+
+always begin
+  #5 clock=~clock;
+end
+
+
+register_file #(16) my_register_file(
+.clk(clock),
+.ra(ra),
+.rb(rb),
+.d(d),
+.writeAddr(writeAddr),
+.writeEnable(writeEnable),
+.reset(reset),
+.a(a),
+.b(b)
+);
+
+endmodule
+
